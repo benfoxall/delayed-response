6ef14ec1efe35eb60660d47247d3df3fd02576df
Ben Foxall <benfoxall@gmail.com>
2017-06-22 18:13:00 -0700

    this code is dreadful

--- delayed-response.js
+++ delayed-response.js
@@ -1,5 +1,33 @@
+importScripts('deps/localforage.min.js')
+
+localforage.config({
+  name        : 'delayed-response',
+  // storeName   : 'scope-' + self.registration.scope
+})
+
 console.log("sw", self)
 
 self.addEventListener('fetch', (e) => {
-  console.log('sw fetch', e.request.url)
+
+  localforage.setItem('request:' + e.request.url, true)
+
+  const original = fetch(e.request)
+
+  e.respondWith(
+    localforage.getItem('delay:' + e.request.url)
+      .then(r => {
+        if(!r) return original
+        else return (new Promise(resolve => {
+          const delay = parseFloat(r) || 0
+          console.log("delaying by ", delay)
+          setTimeout(resolve, delay)
+        })).then(() => {
+          return original
+        })
+      })
+      .catch(e => {
+        console.error(e)
+        return original
+      })
+  )
 })
